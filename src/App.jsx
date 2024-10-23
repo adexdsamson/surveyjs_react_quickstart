c55b08a4c6dd0a447c53c774e155d62d33d05764
Adediran Adeola <35856417+adexdsamson@users.noreply.github.com>
2024-10-23 11:33:39 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useClaim, SurveyClaim } from '@omniguard/claim-ai';
 function App() {
   const claims = useClaim({
     apikey: "FagicNts.VL3HA2L8oEclzYnTlWBAPZWd8kZl8CmN",
-    functionId: "b0640b6e-d803-4e1e-8988-fec49b588d45",
+    functionId: "2e3a5377-745b-46af-b30f-88e28615f12b",
   });
 
 
